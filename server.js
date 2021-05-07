6406a8cbe9fe1807161d7d04c6b9140ac526763b
Amita999 <amitajethani@gmail.com>
2021-05-07 17:11:46 +0530

    mongoose connected

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected'));
 app.listen(port, () => {
   console.log('Listening to server');
 });
