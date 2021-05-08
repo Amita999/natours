385dee0f4056c1e6777514cd64098c5a696cc5ef
Amita999 <amitajethani@gmail.com>
2021-05-08 19:49:47 +0530

    sorting by req.query

--- controllers/tourcontroller.js
+++ controllers/tourcontroller.js
@@ -70,8 +70,10 @@ exports.getAllTours = async (req, res) => {
       const sortBy = req.query.sort
         .split(',')
         .join(' ');
-      console.log(sortBy);
+      // console.log(sortBy);
       query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
     }
     // sort('price ratingsAverage')
     const tours = await query;
